caf915697f5704a874f72d73e3a8655e362e24cc
Paul Wissler <u0791342@utah.edu>
2021-10-29 16:06:31 -0600

    update

--- QuestionAnswers/part2.py
+++ QuestionAnswers/part2.py
@@ -1,41 +1,94 @@
 from pathlib import Path
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 import Perceptron as perc
 
 
 def q2a():
+    print('STANDARD PERCEPTRON')
     X, y = load_bank_note_data('train.csv')
     X_test, y_test = load_bank_note_data('test.csv')
     
     model = perc.PerceptronModel(X, y, random_seed=False, rate=.1)
     error = 1 - model.test(X, y)
-    print(error)
+    print('TRAIN ERROR: ', error)
     error = 1 - model.test(X_test, y_test)
-    print(error)
+    print('TEST ERROR: ', error)
+    print('')
+    print(model.weights)
+    print('\n')
+
+    # STANDARD PERCEPTRON
+    # TRAIN ERROR:  0.04243119266055051
+    # TEST ERROR:  0.050000000000000044
+
+    # WaveletVariance   -5.777450
+    # WaveletSkew       -3.204904
+    # WaveletCurtosis   -4.753204
+    # ImageEntropy      -0.603608
+    # MODEL_BIAS        -5.300000
+    # dtype: float64
 
 
 def q2b():
+    print('VOTED PERCEPTRON')
     X, y = load_bank_note_data('train.csv')
     X_test, y_test = load_bank_note_data('test.csv')
     
     model = perc.VotedPerceptronModel(X, y, random_seed=False, rate=.1)
+    # print(len(model.weights_list))
+    weights_df = pd.DataFrame(model.weights_list)
+    w_cols = list(weights_df.columns)
+    weights_df['VOTES'] = model.votes_list
+    weights_df[w_cols].plot()
+    plt.ylabel('Weights')
+    plt.xlabel('Update #')
+    plt.savefig(Path('Instructions', 'q2b_weights_convergence.png'))
+    plt.close()
+    
+    weights_df.VOTES.plot()
+    plt.ylabel('Vote (Consecutive Rounds Without Failure)')
+    plt.xlabel('Update #')
+    plt.savefig(Path('Instructions', 'q2b_votes.png'))
+    plt.close()
+    
+    # print(len(model.votes_list))
     error = 1 - model.test(X, y)
-    print(error)
+    print('TRAIN ERROR: ', error)
     error = 1 - model.test(X_test, y_test)
-    print(error)
+    print('TEST ERROR: ', error)
+    print('\n')
+
+    # VOTED PERCEPTRON
+    # TRAIN ERROR:  0.012614678899082521
+    # TEST ERROR:  0.014000000000000012
 
 
 def q2c():
+    print('AVERAGE PERCEPTRON')
     X, y = load_bank_note_data('train.csv')
     X_test, y_test = load_bank_note_data('test.csv')
     
     model = perc.AveragePerceptronModel(X, y, random_seed=False, rate=.1)
     error = 1 - model.test(X, y)
-    print(error)
+    print('TRAIN ERROR: ', error)
     error = 1 - model.test(X_test, y_test)
-    print(error)
+    print('TEST ERROR: ', error)
+    print('')
+    print(model.averaged_weights)
+    print('\n')
+
+    # AVERAGE PERCEPTRON
+    # TRAIN ERROR:  0.014908256880733939
+    # TEST ERROR:  0.014000000000000012
+
+    # WaveletVariance   -37108.434439
+    # WaveletSkew       -25185.487162
+    # WaveletCurtosis   -25383.376175
+    # ImageEntropy       -7638.087261
+    # MODEL_BIAS        -34243.400000
 
 
 def load_bank_note_data(csv: str) -> (pd.DataFrame, pd.Series):
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ pd.options.mode.chained_assignment = None  # default='warn'
 
 
 def main():
-    # part2.q2a()
-    # part2.q2b()
+    part2.q2a()
+    part2.q2b()
     part2.q2c()
 
 
